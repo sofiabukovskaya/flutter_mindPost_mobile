fb2f55eb04bc1ed44fc4e3000738c0cdefd2b88b
sofiabukovskaya <sofi454545@gmail.com>
2021-05-24 14:34:03 +0300

    Load image from storage using image package

--- lib/ui/pages/main_page/screens/personal_notes_screen/add_note_page.dart
+++ lib/ui/pages/main_page/screens/personal_notes_screen/add_note_page.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
 import 'dart:ui';
-
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as Path;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -13,6 +16,16 @@ class AddNotePage extends StatefulWidget {
 }
 
 class AddNotePageState extends State<AddNotePage> {
+  File _image;
+  String _uploadedFileUrl;
+
+  Future<void> _pickImage(ImageSource source) async {
+    File selected = await ImagePicker.pickImage(source: source);
+    setState(() {
+      _image = selected;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,55 +51,92 @@ class AddNotePageState extends State<AddNotePage> {
       body: Column(children: [
         Center(
             child: Padding(
-              padding: EdgeInsets.only(top: 10, left: 85, right: 85, bottom: 15),
-              child: TextField(
-                inputFormatters: [
-                  LengthLimitingTextInputFormatter(500),
-                ],
-                style: GoogleFonts.poppins(
-                    fontSize: 20.0,
-                    color: Color(0xFF00847c),
-                    fontWeight: FontWeight.w600),
-                textAlign: TextAlign.center,
-                decoration: InputDecoration(
-                  counterText: '',
-                  hintText: 'Title',
-                  hintStyle: GoogleFonts.poppins(
-                      textStyle: TextStyle(
-                          color: Color(0x4D00847c),
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.w600)),
-                ),
-              ),
-            )),
+          padding: EdgeInsets.only(top: 10, left: 85, right: 85, bottom: 15),
+          child: TextField(
+            inputFormatters: [
+              LengthLimitingTextInputFormatter(500),
+            ],
+            style: GoogleFonts.poppins(
+                fontSize: 20.0,
+                color: Color(0xFF00847c),
+                fontWeight: FontWeight.w600),
+            textAlign: TextAlign.center,
+            decoration: InputDecoration(
+              counterText: '',
+              hintText: 'Title',
+              hintStyle: GoogleFonts.poppins(
+                  textStyle: TextStyle(
+                      color: Color(0x4D00847c),
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600)),
+            ),
+          ),
+        )),
         SingleChildScrollView(
             scrollDirection: Axis.vertical,
             child: SizedBox(
-                width: 350,
-                height: 300,
-                child: TextField(
-                  keyboardType: TextInputType.multiline,
-                  maxLines: 300,
-                  maxLength: 500,
-                  style: GoogleFonts.poppins(
-                      fontSize: 18.0,
-                      color: Colors.black54,
-                      fontWeight: FontWeight.w300),
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Write about your day thoughts, day, desires...',
-                      hintStyle: GoogleFonts.poppins(
-                          textStyle: TextStyle(
-                              color: Color(0x4D00847c),
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.w600)),
-                      isDense: true,
-                      contentPadding:
-                      EdgeInsets.symmetric(vertical: 25, horizontal: 10.0)),
-                ),
-            )
+              width: 350,
+              height: 250,
+              child: TextField(
+                keyboardType: TextInputType.multiline,
+                maxLines: 250,
+                maxLength: 500,
+                style: GoogleFonts.poppins(
+                    fontSize: 18.0,
+                    color: Colors.black54,
+                    fontWeight: FontWeight.w300),
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Write about your day thoughts, day, desires...',
+                    hintStyle: GoogleFonts.poppins(
+                        textStyle: TextStyle(
+                            color: Color(0x4D00847c),
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w600)),
+                    isDense: true,
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 10, horizontal: 10.0)),
+              ),
+            )),
+        Padding(
+          padding: EdgeInsets.only(top: 10, left: 120, right: 90),
+          child: Row(
+            children: [
+              Text(
+                'Attach a picture',
+                style: GoogleFonts.poppins(
+                    fontSize: 18.0, fontWeight: FontWeight.w400),
+              ),
+              IconButton(
+                  icon: Icon(
+                    Icons.photo,
+                    color: Color(0x9900847c),
+                    size: 33,
+                  ),
+                  onPressed: () => _pickImage(ImageSource.gallery)),
+            ],
+          ),
         ),
-      ]),
+        _image == null ?
+          Padding(
+            padding: EdgeInsets.only(top: 10),
+            child: Container(
+              color: Colors.grey,
+              height: 130,
+              width: 180,
+              child: GestureDetector(
+                child: Image.asset(
+                  'assets/camera.png',
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ) : Container(
+          alignment: Alignment.center,
+          child: Image.file(_image, height: 130, width: 180, fit: BoxFit.fill,)
+        )
+      ]
+      ),
     );
   }
 }
