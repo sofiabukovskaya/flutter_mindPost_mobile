9bbad2d940df909014a08de641886bc1cc6437cb
Sofia Bukovskaya <69693280+sofiabukovskaya@users.noreply.github.com>
2021-05-26 02:05:10 +0300

    Create page current note and edit

---
+++ lib/ui/pages/main_page/screens/personal_notes_screen/detail_note_page/detail_note_page.dart
@@ -0,0 +1,140 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class DetailNotePage extends StatefulWidget {
+  final QueryDocumentSnapshot snapshot;
+
+  const DetailNotePage({Key key, this.snapshot}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return DetailNotePageState();
+  }
+}
+
+class DetailNotePageState extends State<DetailNotePage> {
+  TextEditingController controller_text = TextEditingController();
+  TextEditingController controller_desc = TextEditingController();
+  final CollectionReference collectionReferenceNotes =
+  FirebaseFirestore.instance.collection('notes');
+
+  String title ='';
+  String description = '';
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    title = widget.snapshot.get('title').toString();
+    description = widget.snapshot.get('description').toString();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.black),
+        backgroundColor: Colors.white38,
+        elevation: 0,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 30, left: 20),
+              child: ListTile(
+                title: Text(
+                  '$title',
+                  style: GoogleFonts.poppins(
+                      fontSize: 20, fontWeight: FontWeight.w600),
+                  textAlign: TextAlign.center,
+                ),
+                subtitle: Text(
+                  '${widget.snapshot.get('publish').toString()}',
+                  style: GoogleFonts.poppins(
+                      fontSize: 18, fontWeight: FontWeight.w500),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 30, left: 20),
+              child: ListTile(
+                leading: Text(
+                  '$description',
+                  style: GoogleFonts.poppins(
+                      fontSize: 18, fontWeight: FontWeight.w500),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: FlatButton(
+                color: Colors.teal[200],
+                child: Icon(Icons.edit),
+                onPressed: () {
+                  showDialog(context: context, builder: (context) {
+                    return AlertDialog(
+                      title: Text('Change info'),
+                      content: Form(
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              controller: controller_text,
+                              keyboardType: TextInputType.name,
+                              decoration: InputDecoration(
+                                labelText: 'Title:',
+                                hintText: '$title',
+                              ),
+                              onSaved: (String artistValue) {},
+                            ),
+                            TextFormField(
+                              controller: controller_desc,
+                              keyboardType: TextInputType.name,
+                              decoration: InputDecoration(
+                                labelText: 'Description:',
+                                hintText: '$description',
+                              ),
+                              onSaved: (String artistValue) {},
+                            ),
+                          ],
+                        )
+                      ),
+                      actions: [
+                        FlatButton(
+                            child: Text('NO'),
+                            onPressed: () {
+                              Navigator.of(context).pop(false);
+                            }),
+                        FlatButton(
+                            child: Text('YES'),
+                            onPressed: () {
+                              setState(() {
+                                title = controller_text.text;
+                                description = controller_desc.text;
+                              });
+                           collectionReferenceNotes.doc('${widget.snapshot.id}').update({
+                             'title': controller_text.text.isNotEmpty? controller_text.text : title,
+                             'description' : controller_desc.text==''? controller_desc.text : description
+                           })  .then((value) =>
+                                  Scaffold.of(context).showSnackBar(SnackBar(
+                                      content:
+                                      Text("Update ${widget.snapshot.get('title').toString()}"))));
+                              Navigator.of(context).pop(true);
+                            }),
+                      ],
+                    );
+                  });
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/pages/main_page/screens/personal_notes_screen/personal_notes_screen.dart
+++ lib/ui/pages/main_page/screens/personal_notes_screen/personal_notes_screen.dart
@@ -8,6 +8,8 @@ import 'package:flutter_mindpost/ui/pages/main_page/widgets/alert_dialog.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'detail_note_page/detail_note_page.dart';
+
 class PersonalNotesScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -20,7 +22,8 @@ class PersonalNotesScreenState extends State<PersonalNotesScreen> {
   FirestoreRepository firestoreRepository = FirestoreRepository();
   bool public;
   String token = '';
-var fire;
+  var fire;
+
   @override
   void initState() {
     super.initState();
@@ -31,9 +34,13 @@ var fire;
     SharedPreferences prefs = await SharedPreferences.getInstance();
     setState(() {
       token = (prefs.getString('token') ?? '');
-      fire =  FirebaseFirestore.instance.collection('notes').where('userId', isEqualTo: token).snapshots();
+      fire = FirebaseFirestore.instance
+          .collection('notes')
+          .where('userId', isEqualTo: token)
+          .snapshots();
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,7 +77,11 @@ var fire;
           centerTitle: true,
         ),
         floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add, color: Colors.white, size: 45.0,),
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
+            size: 45.0,
+          ),
           backgroundColor: Color(0xFF157C76),
           onPressed: () {
             Navigator.pushNamed(context, '/addNote');
@@ -81,95 +92,112 @@ var fire;
           builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
             if (!snapshot.hasData) {
               return Center(
-                  child: Text('You have not notes, press plus button to add')
+                  child: Text('You have not notes, press plus button to add'));
+            } else if (snapshot.hasData) {
+              return SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Padding(
+                        padding: EdgeInsets.only(top: 15, left: 10, right: 10),
+                        child: textField(searchController, Icon(Icons.search),
+                            Color(0x1A008B83), 'Search by title')),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10, left: 20, right: 20),
+                      child: ListView.builder(
+                          physics: NeverScrollableScrollPhysics(),
+                          scrollDirection: Axis.vertical,
+                          shrinkWrap: true,
+                          itemCount: snapshot.data.docs.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            public = snapshot.data.docs[index]['public'];
+                            return Container(
+                                height: 150,
+                                width: 100,
+                                child: Card(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(15.0)),
+                                  child: Stack(
+                                    children: [
+                                      Positioned(
+                                          top: 20,
+                                          child: Padding(
+                                              padding:
+                                                  EdgeInsets.only(left: 10),
+                                              child: Container(
+                                                height: 100,
+                                                width: 132,
+                                                child: Image.asset(
+                                                    'assets/giraffe.png'),
+                                              ))),
+                                      Positioned(
+                                        top: 20,
+                                        left: 150,
+                                        child: Text(
+                                            '${snapshot.data.docs[index]['publish'].toString()}',
+                                            style: GoogleFonts.poppins(
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.w400)),
+                                      ),
+                                      Positioned(
+                                        top: 46,
+                                        left: 150,
+                                        child: Text(
+                                            '${snapshot.data.docs[index]['title']}',
+                                            style: GoogleFonts.poppins(
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.w600)),
+                                      ),
+                                      Positioned(
+                                          top: 70,
+                                          left: 150,
+                                          child: GestureDetector(
+                                            onTap: () => navigateToDeatail(snapshot.data.docs[index]),
+                                            child:Container(
+                                                height: 150,
+                                                width: 200,
+                                                child: Text(
+                                                    '${snapshot.data.docs[index]['description'].toString()}',
+                                                    maxLines: 2,
+                                                    overflow:
+                                                    TextOverflow.ellipsis,
+                                                    style: GoogleFonts.poppins(
+                                                        fontSize: 18,
+                                                        fontWeight:
+                                                        FontWeight.w400))) ,
+                                          )
+                                          ),
+                                      Positioned(
+                                          top: 50,
+                                          left: 300,
+                                          child: public == true
+                                              ? Icon(
+                                                  Icons.lock_open,
+                                                  color: Colors.black38,
+                                                )
+                                              : Icon(
+                                                  Icons.lock_outline,
+                                                  color: Colors.black38,
+                                                ))
+                                    ],
+                                  ),
+                                ));
+                          }),
+                    ),
+                  ],
+                ),
               );
-            } else if(snapshot.hasData) {
-           return SingleChildScrollView(
-            child: Column(
-            children: [
-            Padding(
-            padding: EdgeInsets.only(top: 15, left: 10, right: 10),
-            child: textField(searchController, Icon(Icons.search),
-            Color(0x1A008B83), 'Search by title')),
-            Padding(
-            padding: EdgeInsets.only(top: 10, left: 20, right: 20),
-            child: ListView.builder(
-            physics: NeverScrollableScrollPhysics(),
-            scrollDirection: Axis.vertical,
-            shrinkWrap: true,
-            itemCount: snapshot.data.docs.length,
-            itemBuilder: (BuildContext context, int index) {
-              public = snapshot.data.docs[index]['public'];
-            return Container(
-            height: 150,
-            width: 100,
-            child: Card(
-            shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15.0)),
-            child: Stack(
-            children: [
-            Positioned(
-            top: 20,
-            child: Padding(
-            padding: EdgeInsets.only(left: 10),
-            child: Container(
-            height: 100,
-            width: 132,
-            child:
-            Image.asset('assets/giraffe.png'),
-            ))),
-            Positioned(
-            top: 20,
-            left: 150,
-            child: Text('${snapshot.data.docs[index]['publish'].toString()}',
-            style: GoogleFonts.poppins(
-            fontSize: 18,
-            fontWeight: FontWeight.w400)),
-            ),
-            Positioned(
-            top: 46,
-            left: 150,
-            child: Text('${snapshot.data.docs[index]['title']}',
-            style: GoogleFonts.poppins(
-            fontSize: 18,
-            fontWeight: FontWeight.w600)),
-            ),
-            Positioned(
-            top: 70,
-            left: 150,
-            child: Container(
-            height: 150,
-            width: 200,
-            child: Text('${snapshot.data.docs[index]['description'].toString()}',
-            maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-            style: GoogleFonts.poppins(
-            fontSize: 18,
-            fontWeight: FontWeight.w400)))
-            ),
-            Positioned(
-            top: 60,
-            left: 300,
-            child: public == true ? Icon(Icons.lock_open, color: Colors.black38,) : Icon(Icons.lock_outline, color: Colors.black38,)
-            )
-            ],
-            ),
-
-            ));
-            }),
-            ),
-            ],
-            ),
-            );
             }
             return Center(
-            child: CircularProgressIndicator(
-            valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
-            ),
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+              ),
             );
           },
-        )
+        ));
+  }
 
-    );
+  navigateToDeatail(QueryDocumentSnapshot doc) {
+    Navigator.push(context, MaterialPageRoute(builder: (context)=> DetailNotePage(snapshot: doc)));
   }
 }
